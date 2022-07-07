3780020b6c30bf26588f3a3cf55c8c06b94587cf
feliks.pobiedzinski@swmansion.com <feliks.pobiedzinski@swmansion.com>
2022-07-07 14:40:46 +0200

    Ensure Posgrex started

--- lib/membrane_rtc_engine_timescaledb.ex
+++ lib/membrane_rtc_engine_timescaledb.ex
@@ -4,7 +4,12 @@ defmodule Membrane.RTC.Engine.TimescaleDB.App do
 
   @impl true
   def start(_start_type, _start_args) do
-    children = [{Membrane.RTC.Engine.TimescaleDB.Repo, []}]
+    Application.ensure_all_started(:postgrex)
+
+    children = [
+      {Membrane.RTC.Engine.TimescaleDB.Repo, [show_sensitive_data_on_connection_error: true]}
+    ]
+
     Supervisor.start_link(children, strategy: :one_for_one)
   end
 end
