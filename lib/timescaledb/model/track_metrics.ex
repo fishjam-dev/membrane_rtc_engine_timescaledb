29e39566292481c69ef959dd9a34692eef933336
Daniel Jodłoś <daniel.jodlos@swmansion.com>
2023-03-17 13:28:26 +0100

    WIP

--- lib/timescaledb/model/track_metrics.ex
+++ lib/timescaledb/model/track_metrics.ex
@@ -19,6 +19,23 @@ defmodule Membrane.RTC.Engine.TimescaleDB.Model.TrackMetrics do
           "inbound-rtp.packets": non_neg_integer() | nil,
           "inbound-rtp.frames": non_neg_integer() | nil,
           "inbound-rtp.keyframes": non_neg_integer() | nil,
+          "rtcp.sender_reports_received": non_neg_integer() | nil,
+          "rtcp.total_packets_received": non_neg_integer() | nil,
+          "inbound-rtp.markers_received": non_neg_integer() | nil,
+          "outbound-rtp.markers_sent": non_neg_integer() | nil,
+          "rtcp.total_packets_sent": non_neg_integer() | nil,
+          "rtcp.nack_sent": non_neg_integer() | nil,
+          "rtcp.fir_sent": non_neg_integer() | nil,
+          "rtcp.sender_reports_sent": non_neg_integer() | nil,
+          "rtcp.recevier_reports_sent": non_neg_integer() | nil,
+          "rtcp.nack_received": non_neg_integer() | nil,
+          "rtcp.fir_received": non_neg_integer() | nil,
+          "rtcp.pli_received": non_neg_integer() | nil,
+          "rtcp.sender_reports_received": non_neg_integer() | nil,
+          "rtcp.recevier_reports_received": non_neg_integer() | nil,
+          "outbound-rtp.rtx_sent": non_neg_integer() | nil,
+          "outbound-rtp.packets": non_neg_integer() | nil,
+          "outbound-rtp.bytes": non_neg_integer() | nil,
           peer_id: String.t() | nil,
           peer_metrics_id: integer() | nil,
           peer_metrics: PeerMetrics.t() | Ecto.Association.NotLoaded.t(),
@@ -35,6 +52,23 @@ defmodule Membrane.RTC.Engine.TimescaleDB.Model.TrackMetrics do
     field :"inbound-rtp.packets", :integer
     field :"inbound-rtp.frames", :integer
     field :"inbound-rtp.keyframes", :integer
+    field :"rtcp.sender_reports_received", :integer
+    field :"rtcp.total_packets_received", :integer
+    field :"inbound-rtp.markers_received", :integer
+    field :"outbound-rtp.markers_sent", :integer
+    field :"rtcp.total_packets_sent", :integer
+    field :"rtcp.nack_sent", :integer
+    field :"rtcp.fir_sent", :integer
+    field :"rtcp.sender_reports_sent", :integer
+    field :"rtcp.recevier_reports_sent", :integer
+    field :"rtcp.nack_received", :integer
+    field :"rtcp.fir_received", :integer
+    field :"rtcp.pli_received", :integer
+    field :"rtcp.sender_reports_received", :integer
+    field :"rtcp.recevier_reports_received", :integer
+    field :"outbound-rtp.rtx_sent", :integer
+    field :"outbound-rtp.packets", :integer
+    field :"outbound-rtp.bytes", :integer
 
     timestamps type: :utc_datetime_usec, updated_at: false
 
@@ -56,7 +90,24 @@ defmodule Membrane.RTC.Engine.TimescaleDB.Model.TrackMetrics do
           :"inbound-rtp.keyframe_request_sent",
           :"inbound-rtp.packets",
           :"inbound-rtp.frames",
-          :"inbound-rtp.keyframes"
+          :"inbound-rtp.keyframes",
+          :"rtcp.sender_reports_received",
+          :"rtcp.total_packets_received",
+          :"inbound-rtp.markers_received",
+          :"outbound-rtp.markers_sent",
+          :"rtcp.total_packets_sent",
+          :"rtcp.nack_sent",
+          :"rtcp.fir_sent",
+          :"rtcp.sender_reports_sent",
+          :"rtcp.recevier_reports_sent",
+          :"rtcp.nack_received",
+          :"rtcp.fir_received",
+          :"rtcp.pli_received",
+          :"rtcp.sender_reports_received",
+          :"rtcp.recevier_reports_received",
+          :"outbound-rtp.rtx_sent",
+          :"outbound-rtp.packets",
+          :"outbound-rtp.bytes"
         ]
 
     schema
