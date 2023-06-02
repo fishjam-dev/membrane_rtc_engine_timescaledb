c114f4af99301f03b003a2a95182169399d52357
Bartosz Błaszków <bartosz.blaszkow@swmansion.com>
2023-06-02 17:19:06 +0200

    Add versioning when running TimescaleDB.Migrations

--- lib/timescaledb/migrations/v02.ex
+++ lib/timescaledb/migrations/v02.ex
@@ -1,11 +1,20 @@
 defmodule Membrane.RTC.Engine.TimescaleDB.Migrations.V02 do
   @moduledoc false
 
+  @behaviour Membrane.RTC.Engine.TimescaleDB.Migration
+
   use Ecto.Migration
 
-  @spec change() :: :ok
-  def change() do
+  @impl true
+  def up() do
     create index(:tracks_metrics, [:peer_metrics_id])
+
+    :ok
+  end
+
+  @impl true
+  def down() do
+    drop index(:tracks_metrics, [:peer_metrics_id])
 
     :ok
   end
